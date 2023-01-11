3bc02b9cbdf77e447d698ee54cae5931313ad8b9
Okraks <antwisaac7@gmail.com>
2023-01-11 19:29:42 +0000

    landing

--- src/app/Home/Home.js
+++ src/app/Home/Home.js
@@ -10,6 +10,26 @@ function Home() {
     <div>
       <Navbar />
       <Header />
+
+      {/* Features  */}
+      <div className="bg-secondary flex justify-around py-20 items-center">
+        <div>
+          <h3>Quality Service</h3>
+          <p>
+            Join our community and take control of your income or find reliable
+            professionals today
+          </p>
+        </div>
+        <div>
+          <h3>Secure and safe</h3>
+          <p>Get access to verified, top-rated, and trusted handypersons</p>
+        </div>
+        <div>
+          <h3>Easy to use</h3>
+          <p>Get your home and work projects completed with a few clicks</p>
+        </div>
+      </div>
+
       <CTA />
       <Footer />
     </div>
--- src/components/CTA/CTA.js
+++ src/components/CTA/CTA.js
@@ -2,15 +2,15 @@ import React from "react";
 
 function CTA() {
   return (
-    <div>
-      <h3>Get started with Handys today!</h3>
-      <p>
+    <div className="py-20 bg-primary text-white text-center">
+      <h3 className="text-4xl">Get started with Handys today!</h3>
+      <p className="py-5">
         Join our community and take control of your income or find reliable
         professionals today
       </p>
       <div>
-        <button>Provide Services</button>
-        <button>Get Services</button>
+        <button className="btn-white mr-5">Provide Services</button>
+        <button className="btn-white-outline">Get Services</button>
       </div>
     </div>
   );
