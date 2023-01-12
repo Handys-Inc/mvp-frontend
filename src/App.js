1184fc4a1a9f07ba351eef6bc0f06f8cc154d617
Okraks <antwisaac7@gmail.com>
2023-01-12 19:53:31 +0000

    Sign up Introductin

--- src/App.js
+++ src/App.js
@@ -6,14 +6,23 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // general app pages
 import Home from "./app/Home/Home";
 import Notfound from "./app/NotFound/Notfound";
+import LogIn from "./app/Auth/LogIn.js/LogIn";
+import SignUp from "./app/Auth/SignUp/SignUp";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/*Routes */}
-
+        {/*General Routes */}
         <Route path="/" exact element={<Home />} />
+
+        {/* Auth Routes for Sign up and Login (Client and Provider) */}
+        <Route path="/login" exact element={<LogIn />} />
+        <Route path="/signup" exact element={<SignUp />} />
+
+        {/* Specific Auth Routes: Client */}
+
+        {/* Specific Auth Routes: Provider */}
 
         {/* PAGE NOT FOUND */}
         <Route path="*" exact element={<Notfound />} />
