314e7f26318812410fe17d56df6af60c8964d942
Okraks <antwisaac7@gmail.com>
2023-01-12 18:52:14 +0000

    UI Edits

--- src/app/Home/components/Header.js
+++ src/app/Home/components/Header.js
@@ -7,50 +7,64 @@ import HammerIcon from "../../../assets/icons/svg/HammerIcon";
 import WandIcon from "../../../assets/icons/svg/WandIcon";
 
 // man
-import man from "../../../assets/img/workers.png";
 
 // components
 import SignUp from "./SignUp";
 import Search from "./Search";
+
+// custom css
+import "./index.css";
 
 function Header() {
   return (
     <div className="bg-none md:bg-workerHero md:min-h-[80vh] bg-no-repeat bg-cover flex flex-col md:flex-row items-center md:px-20 mx-auto">
       <div className="bg-secondary md:rounded-2xl w-full md:w-[35rem] md:h-[25rem]">
         <Tab.Group>
-          <Tab.List className="flex justify-around border-b border-white outline-none pt-4">
-            <Tab as={Fragment}>
-              {({ selected }) => (
-                <button
-                  className={
-                    selected
-                      ? "inline-flex p-4 text-primary border-b-2 border-primary focus:outline-none"
-                      : "inline-flex p-4 text-gray border-b-2 border-none"
-                  }
-                >
-                  <HammerIcon
-                    className="mr-5"
-                    fill={selected ? "#CE1449" : "#626262"}
-                  />
-                  <span className="ml-2">Provide Service</span>
-                </button>
-              )}
-            </Tab>
-            <Tab as={Fragment}>
-              {({ selected }) => (
-                <button
-                  className={
-                    selected
-                      ? "inline-flex p-4 text-primary border-b-2 border-primary  focus:outline-none"
-                      : "inline-flex p-4 text-gray border-b-2 border-none  "
-                  }
-                >
-                  <WandIcon fill={selected ? "#CE1449" : "#626262"} />{" "}
-                  <span className="ml-2">Get Services</span>
-                </button>
-              )}
-            </Tab>
-          </Tab.List>
+          <div class="border-b border-white">
+            <Tab.List class="flex flex-wrap justify-around -mb-px pt-4">
+              <Tab as={Fragment}>
+                {({ selected }) => (
+                  <button
+                    className={
+                      selected
+                        ? "inline-flex p-4 text-primary border-b-2 border-primary  focus:outline-none"
+                        : "inline-flex p-4 text-gray border-b-2 border-none"
+                    }
+                  >
+                    <HammerIcon
+                      className="mr-5"
+                      fill={selected ? "#CE1449" : "#626262"}
+                    />
+                    <span className="ml-2">Provide Service</span>
+                  </button>
+                )}
+              </Tab>
+              <Tab as={Fragment}>
+                {({ selected }) => (
+                  <button
+                    className={
+                      selected
+                        ? "inline-flex p-4 text-primary border-b-2 border-primary  focus:outline-none"
+                        : "inline-flex p-4 text-gray border-b-2 border-none  "
+                    }
+                  >
+                    <WandIcon fill={selected ? "#CE1449" : "#626262"} />{" "}
+                    <span className="ml-2">Get Services</span>
+                  </button>
+                )}
+              </Tab>
+              {/* <li class="mr-2">
+                <span class=" hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">
+                  Profile
+                </span>
+              </li>
+              <li class="mr-2">
+                <span class="inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active ">
+                  Dashboard
+                </span>
+              </li> */}
+            </Tab.List>
+          </div>
           <Tab.Panels className="px-5 md:px-10">
             <Tab.Panel>
               <SignUp />
@@ -62,8 +76,13 @@ function Header() {
         </Tab.Group>
       </div>
 
-      <div className="block md:hidden  w-full bg-secondary">
-        <img src={man} alt="workers" className="w-full" />
+      {/* <Tab.Group>
+        <Tab.List className=" custom-border justify-around border-b border-white outline-none pt-4"></Tab.List>
+    
+      </Tab.Group> */}
+
+      <div className="block md:hidden  w-full bg-secondary bg-workerHero h-[22rem] bg-cover bg-center">
+        {/* <img src={man} alt="workers" className="w-full h-[20rem]" /> */}
       </div>
     </div>
   );
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-import logo from "../../assets/logo/logo.png";
+import logo from "../../assets/logo/logo.svg";
 
 function Navbar() {
   return (
     <div className="flex justify-between items-center px-2 md:px-20 py-2 mx-auto">
       <div>
-        <img src={logo} alt="Logo" />
+        <img className="w-20 md:w-28" src={logo} alt="Logo" />
       </div>
       <div>
         <button className="btn-hollow mr-5 md:mr-10">Log In</button>
